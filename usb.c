35cf548e6785131a7c8edd8b5ce9736d82cca2dd
David Griffith <dave@661.org>
2019-06-25 04:25:02 -0700

    Move usb timeout to a define.

--- usb.c
+++ usb.c
@@ -26,6 +26,7 @@
 #define MP_TL866II_VID 0xa466
 #define MP_TL866II_PID 0x0a53
 #define MP_TL866IIPLUS 5
+#define MP_USBTIMEOUT 20000
 
 // Open usb device
 libusb_device_handle *usb_open() {
@@ -120,7 +121,7 @@ static int msg_transfer(libusb_device_handle *handle, uint8_t *buffer,
                         size_t size, uint8_t direction, uint8_t endpoint,
                         int *bytes_transferred) {
   int ret = libusb_bulk_transfer(handle, (endpoint | direction), buffer, size,
-                                 bytes_transferred, 20000);
+                                 bytes_transferred, MP_USBTIMEOUT);
 
   if (ret != LIBUSB_SUCCESS)
     fprintf(stderr, "\nIO error: bulk_transfer: %s\n", libusb_error_name(ret));
@@ -145,10 +146,10 @@ static int payload_transfer(libusb_device_handle *handle, uint8_t direction,
 
   libusb_fill_bulk_transfer(ep2_urb, handle, (0x02 | direction), ep2_buffer,
                             ep2_length, payload_transfer_cb, &ep2_completed,
-                            20000);
+                            MP_USBTIMEOUT);
   libusb_fill_bulk_transfer(ep3_urb, handle, (0x03 | direction), ep3_buffer,
                             ep3_length, payload_transfer_cb, &ep3_completed,
-                            20000);
+                            MP_USBTIMEOUT);
 
   ret = libusb_submit_transfer(ep2_urb);
   if (ret < 0) {
