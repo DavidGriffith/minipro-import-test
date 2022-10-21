22ec014f6bf0c45741d68d03c99489d0b852ff06
romanstingler <coolx67@gmx.at>
2022-10-21 16:00:42 +0000

    Update tl866iiplus.h (V12.32  2022.10.12)

--- tl866iiplus.h
+++ tl866iiplus.h
@@ -18,8 +18,8 @@
 #ifndef __TL866IIPLUS_H
 #define __TL866IIPLUS_H
 
-#define TL866IIPLUS_FIRMWARE_VERSION 0x283
-#define TL866IIPLUS_FIRMWARE_STRING  "04.2.131"
+#define TL866IIPLUS_FIRMWARE_VERSION 0x284
+#define TL866IIPLUS_FIRMWARE_STRING  "04.2.132"
 
 // TL866II+ low level functions.
 int tl866iiplus_begin_transaction(minipro_handle_t *handle);
